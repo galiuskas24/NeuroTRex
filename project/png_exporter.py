50cf70f7a83d2aa8c74361639e26f97ae3abe314
Marino Voćanec <51049764+MarinoVocanec@users.noreply.github.com>
2020-10-20 12:15:25 +0200

    PNG export added

---
+++ project/png_exporter.py
@@ -0,0 +1,106 @@
+from PIL import Image, ImageDraw
+
+
+class PngExporter:
+
+    def __init__(self,
+                 width=800,
+                 height=600,
+                 abs_max_weight=1.0,
+                 abs_min_weight=0.1,
+                 connection_width_part=0.01,
+                 show_disabled=False,
+                 node_size_part=0.05,
+                 node_outline_width_part=0.05,
+                 rescale_factor=0.95,
+                 background_color=(255, 255, 255),
+                 negative_weight_color=(255, 0, 0),
+                 positive_weight_color=(0, 255, 0),
+                 disabled_weight_color=(192, 192, 192),
+                 node_fill_color=(168, 168, 168),
+                 node_outline_color=(32, 32, 32)):
+        self.width = width
+        self.height = height
+        self.abs_max_weight = abs_max_weight
+        self.abs_min_weight = abs_min_weight
+        self.connection_width_part = connection_width_part
+        self.show_disabled = show_disabled
+        self.node_size_part = node_size_part
+        self.node_outline_width_part = node_outline_width_part
+        self.rescale_factor = rescale_factor
+        self.background_color = background_color
+        self.negative_weight_color = negative_weight_color
+        self.positive_weight_color = positive_weight_color
+        self.disabled_weight_color = disabled_weight_color
+        self.node_fill_color = node_fill_color
+        self.node_outline_color = node_outline_color
+
+    def _separate_layers(self, genome):
+        layers = {}
+        for node in genome.nodes:
+            layer = node.layer
+            if layer not in layers:
+                layers[layer] = []
+            layers[layer].append(node)
+        for layer in layers:
+            layers[layer].sort(key=lambda n: n.id)
+        return layers
+
+    def _determine_weight_color(self, connection):
+        if not connection.enabled:
+            return self.disabled_weight_color
+        return self.positive_weight_color if connection.weight > 0 else self.negative_weight_color
+
+
+    def _resolution_error(self, message):
+        raise Exception(f"Resolution is too small. {message}")
+
+    def export(self, genome, name):
+        image = Image.new("RGB", (self.width, self.height), color=self.background_color)
+        d = ImageDraw.Draw(image)
+
+        default_node_size = int(self.width * self.node_size_part)
+        if default_node_size < 1:
+            self._resolution_error("Default node size is not visible.")
+        max_connection_width = int(self.width * self.connection_width_part)
+        if max_connection_width < 1:
+            self._resolution_error("Maximal connection width is not visible.")
+
+        offset_x = default_node_size
+        layers = self._separate_layers(genome)
+        layers_distance = (self.width - 2 * offset_x) // (len(layers) - 1)
+        if layers_distance < 1:
+            self._resolution_error("Distance between layers is not visible.")
+
+        node_data = {}
+        for layer in layers:
+            current_x = offset_x + layer * layers_distance
+            current_nodes = layers[layer]
+            for i, node in enumerate(current_nodes):
+                nodes_distance = self.height // (len(current_nodes) + 1)
+                if nodes_distance < 1:
+                    self._resolution_error("Distance between nodes is not visible.")
+                current_node_size = min(default_node_size, layers_distance, nodes_distance)
+                if current_node_size < default_node_size:
+                    current_node_size = int(current_node_size * self.rescale_factor)
+                if current_node_size < 1:
+                    self._resolution_error("Nodes are not visible.")
+                node_data[node.id] = (current_x, (i+1) * nodes_distance, current_node_size)
+
+        for connection in genome.connections:
+            if not connection.enabled and not self.show_disabled: continue
+            in_node_x, in_node_y, _ = node_data[connection.in_node_id]
+            out_node_x, out_node_y, _ = node_data[connection.out_node_id]
+            points = [(in_node_x, in_node_y), (out_node_x, out_node_y)]
+            bounded_weight = min(max(abs(connection.weight), self.abs_min_weight), self.abs_max_weight)
+            width = int(bounded_weight / self.abs_max_weight * max_connection_width)
+            color = self._determine_weight_color(connection)
+            d.line(points, width=width, fill=color)
+
+        for node_x, node_y, size in node_data.values():
+            radius = size // 2
+            points = [(node_x - radius, node_y - radius), (node_x + radius, node_y + radius)]
+            width = int(size * self.node_outline_width_part)
+            d.ellipse(points, outline=self.node_outline_color, fill=self.node_fill_color, width=width)
+
+        image.save(name)
---
+++ project/png_exporter_test.py
@@ -0,0 +1,63 @@
+from neat.activations import Identity, ReLU
+from neat.connection import Connection
+from neat.genome import Genome
+from neat.node import Node
+from png_exporter import PngExporter
+
+genome = Genome()
+genome.add_node(Node(1, 0, Identity(), 0.0))
+genome.add_node(Node(2, 0, Identity(), 0.0))
+genome.add_node(Node(3, 0, Identity(), 0.0))
+
+genome.add_node(Node(6, 1, ReLU(), 1.0))
+genome.add_node(Node(7, 1, ReLU(), 1.0))
+genome.add_node(Node(8, 1, ReLU(), 1.0))
+genome.add_node(Node(9, 1, ReLU(), 1.0))
+genome.add_node(Node(10, 1, ReLU(), 1.0))
+
+genome.add_node(Node(11, 2, ReLU(), 1.0))
+genome.add_node(Node(12, 2, ReLU(), 1.0))
+genome.add_node(Node(13, 2, ReLU(), 1.0))
+genome.add_node(Node(14, 2, ReLU(), 1.0))
+
+genome.add_node(Node(15, 3, ReLU(), 1.0))
+genome.add_node(Node(16, 3, ReLU(), 1.0))
+genome.add_node(Node(17, 3, ReLU(), 1.0))
+
+genome.add_node(Node(4, 4, ReLU(), 1.0))
+genome.add_node(Node(5, 4, ReLU(), 1.0))
+
+genome.add_connection(Connection(1, 1, 6, True, 0, 0.5))
+genome.add_connection(Connection(1, 1, 7, True, 0, -1.0))
+genome.add_connection(Connection(1, 1, 9, True, 0, -0.4))
+
+genome.add_connection(Connection(1, 2, 6, True, 0, 0.7))
+genome.add_connection(Connection(1, 2, 7, True, 0, -0.1))
+
+genome.add_connection(Connection(1, 3, 6, False, 0, 0.6))
+genome.add_connection(Connection(1, 3, 9, True, 0, -0.18))
+genome.add_connection(Connection(1, 3, 8, True, 0, 0.72))
+genome.add_connection(Connection(1, 3, 7, True, 0, -0.9))
+
+genome.add_connection(Connection(1, 6, 11, True, 0, 0.6))
+genome.add_connection(Connection(1, 6, 13, False, 0, -0.5))
+genome.add_connection(Connection(1, 7, 14, True, 0, 1.0))
+genome.add_connection(Connection(1, 7, 12, True, 0, -0.9))
+genome.add_connection(Connection(1, 8, 11, True, 0, 0.7))
+genome.add_connection(Connection(1, 9, 13, True, 0, -0.7))
+genome.add_connection(Connection(1, 9, 12, False, 0, -0.01))
+genome.add_connection(Connection(1, 10, 13, False, 0, -0.7))
+
+genome.add_connection(Connection(1, 11, 15, True, 0, -0.62))
+genome.add_connection(Connection(1, 12, 15, False, 0, -0.28))
+genome.add_connection(Connection(1, 13, 16, True, 0, 0.42))
+genome.add_connection(Connection(1, 14, 17, True, 0, -0.95))
+genome.add_connection(Connection(1, 11, 4, True, 0, -0.2))
+genome.add_connection(Connection(1, 12, 5, False, 0, -0.1))
+genome.add_connection(Connection(1, 14, 4, True, 0, -0.5))
+genome.add_connection(Connection(1, 13, 4, True, 0, 0.32))
+genome.add_connection(Connection(1, 16, 4, False, 0, -0.4))
+genome.add_connection(Connection(1, 16, 5, True, 0, 0.82))
+
+exporter = PngExporter(width=1920, height=1080, show_disabled=True)
+exporter.export(genome, "test-export.png")
